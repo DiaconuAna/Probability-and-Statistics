5724c01f22f049cf8bab964d487fac3adf0bfb3d
Diaconu Ana-Maria <daie2909@scs.ubbcluj.ro>
2022-01-07 11:07:40 +0200

    Create contents.m

---
+++ contents.m
@@ -0,0 +1,24 @@
+% Table of contents of some sorts
+
+% Laboratory 5
+
+% 1. a => confidence interval for a population mean with sigma known
+% 1. b => confidence interval for a population mean with sigma unknown
+% 1. c => confidence interval for a population variance (and standard
+% deviation)
+
+% 2. a => confidence inverval for the difference of the true means with
+% sigma_1 = sigma_2, unknown
+% 2. b => confidence interval for the difference of the true means with
+% sigma_1 != sigma_2, unknown
+% 2. c => confidence interval for the ratio of the variances
+
+% Laboratory 6
+
+% 1. a => left-tailed test for the mean (sigma known) - ztest, norminv
+% 1. b => right-tailed test for the mean (sigma unknown) - ttest, tinv
+
+% 2. a => two-tailed test for the population variances - vartest2, finv
+% 2. b => right-tailed test for the difference of means - ttest2, tinv
+
+
